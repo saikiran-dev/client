8a2589421db6245206cc730b1f05404ac5ae42de
saikiran-dev <msaikirandeveloper@gmail.com>
2023-05-23 22:57:44 +0530

    Introduction of properties for each element and decision over CSS framework

---
+++ src/components/content/Properties.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+function Properties() {
+  const [activeTab, setActiveTab] = useState();
+  return (
+    <div>
+      <ul>
+        <li>Properties</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Properties;
